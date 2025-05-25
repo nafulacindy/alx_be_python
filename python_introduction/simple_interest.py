911a19ec431689620d77432ccb7366d12c380995
nafulacindy <cindytalia022@gmail.com>
2025-05-25 09:54:53 +0300

    debug basic_operations.py with basic arithmetic operations

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,7 @@
-principle = 1000
+principal = 1000
 rate = 0.05
 time = 3
-interest = principle * rate * time
-print (f"The simple interesdt is : {interest}")
 
+interest = principal * rate * time
+
+print("The simple interest is: {interest}")
